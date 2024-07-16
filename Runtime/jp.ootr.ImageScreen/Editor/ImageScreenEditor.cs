544da37cba3db63eddef255d2b62e531d8ad3d4e
ootr <git@ootr.jp>
2024-07-16 17:05:21 +0900

    初期化

---
+++ Runtime/jp.ootr.ImageScreen/Editor/ImageScreenEditor.cs
@@ -0,0 +1,36 @@
+﻿using jp.ootr.common;
+using UnityEditor;
+
+namespace jp.ootr.ImageScreen.Editor
+{
+    [CustomEditor(typeof(ImageScreen))]
+    public class ImageScreenEditor : UnityEditor.Editor
+    {
+        private bool debug;
+        public override void OnInspectorGUI()
+        {
+            var script = (ImageScreen)target;
+            
+            debug = EditorGUILayout.ToggleLeft("Debug", debug);
+            if (debug)
+            {
+                base.OnInspectorGUI();
+                return;
+            }
+
+            EditorGUILayout.LabelField("ImageScreen", EditorStyle.UiTitle);
+
+            EditorGUILayout.Space();
+
+            EditorGUILayout.LabelField("Device Name");
+            var lastDeviceName = script.deviceName;
+            script.deviceName = EditorGUILayout.TextField(script.deviceName);
+            script.inputField.text = script.deviceName;
+
+            if (lastDeviceName != script.deviceName)
+            {
+                EditorUtility.SetDirty(script);
+            }
+        }
+    }
+}
